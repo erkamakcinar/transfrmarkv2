7508f5bdb510e82cb70183fcdea5ad0a9f483bb8
Onur1258 <onur.deniz_1999@hotmail.com>
2022-08-14 20:22:45 +0300

    fill def_table

--- packet/backend/server.py
+++ packet/backend/server.py
@@ -1,18 +1,23 @@
+import imp
 from flask import render_template, request
 from packet.backend import app
 from packet.ui.filter import TeamFiler, PlayerFiler
 from packet.backend.requestHandler import handler
+from packet.backend import def_table
 
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    teams = ()
+    table_elements = def_table
+    print(table_elements)
     teamFilter = TeamFiler()
     playerFilter = PlayerFiler()
+    if request.method == "GET":
+        return render_template("home.html", title='Best Football Site', table_elements=table_elements, teamFilter=teamFilter, playerFilter=playerFilter)
     if request.method == "POST":
         print(request.form)
         obj = handler()
-        teams = obj.tableBuilder(request.form)
-        print(teams)
-        return render_template("home.html", title='Best Football Site', teams=teams, teamFilter=teamFilter, playerFilter=playerFilter)
-    return render_template("home.html", title='Best Football Site', teams=teams, teamFilter=teamFilter, playerFilter=playerFilter)
+        table_elements = obj.tableBuilder(request.form)
+        print(table_elements)
+        return render_template("home.html", title='Best Football Site', table_elements=table_elements, teamFilter=teamFilter, playerFilter=playerFilter)
+    return render_template("home.html", title='Best Football Site', table_elements=table_elements, teamFilter=teamFilter, playerFilter=playerFilter)
