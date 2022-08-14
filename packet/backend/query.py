3d747cffbe798f5482b29d14bf1bdb70759fea90
Erkam AKÇINAR <erkamakcinar@Erkam-MacBook-Pro.local>
2022-08-15 02:02:13 +0300

    1

--- packet/backend/query.py
+++ packet/backend/query.py
@@ -3,12 +3,18 @@ from packet.backend import cursor
 
 class Query:
     def __init__(self, queryElements):
-        self.qE = queryElements
+        self.queryElements = queryElements
         self.cursor = cursor
     
-    def queryBuilder(self):
-        query = """ select * from TAKIM t where (t.isim = %s)"""
-        tp = (self.qE)
+    def queryBuilder(self, isQueryAboutTeam):
+        print(self.queryElements)
+        if(isQueryAboutTeam == True):
+            team_name = self.queryElements['team_name']
+            cup_num = self.queryElements['cup_num']
+            total_val = self.queryElements['total_val']
+            home_towns = self.queryElements['home_towns']
+            query = """ select * from TAKIM t where (t.isim = %s)"""
+            tp = (team_name)
         cursor.executemany(query, [tp])
         cursor.connection.commit()
         return cursor.fetchall()
