a1365d4c36da9ba5dd8f472152671ba0ad91d2a5
Erkam AKÇINAR <erkamakcinar@Erkam-MBP.home>
2022-08-15 17:16:33 +0300

    Team filtering basically done

--- packet/backend/query.py
+++ packet/backend/query.py
@@ -1,4 +1,5 @@
 
+from tkinter.messagebox import QUESTION
 from packet.backend import cursor
 
 class Query:
@@ -8,17 +9,162 @@ class Query:
     
     def queryBuilder(self, isQueryAboutTeam):
         if(isQueryAboutTeam == True):
+            result = []
             team_name = self.queryElements['team_name']
             cup_num = self.queryElements['cup_num']
             total_val = self.queryElements['total_val']
             home_towns = self.queryElements['home_towns']
-            query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
-                        FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
-                            inner join OYUNCU `o` on (t.Isim = o.Takim)
-                                inner join INSAN `i` on(i.Tc = o.OyuncuTc)
-                        GROUP BY (t.Isim)
-                    """
-            cursor.execute(query)
+            
+            if(not team_name and not cup_num and not total_val and not home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim)
+                        """
+                cursor.execute(query)
+
+            if(team_name and not cup_num and not total_val and not home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (t.Isim = %s)
+                        """
+                print(query)        
+                obj = (team_name)
+                cursor.execute(query,obj)
+
+            if(not team_name and cup_num and not total_val and not home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (kupaSayisi = %s)
+                        """
+                obj = (cup_num)
+                cursor.execute(query,obj)
+
+            if(not team_name and not cup_num and total_val and not home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (piyasaDegeri = %s)
+                        """
+                obj = (total_val)
+                cursor.execute(query,obj) 
+            
+            if(not team_name and not cup_num and not total_val and home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (t.Sehir = %s)
+                        """
+                obj = (home_towns)
+                cursor.execute(query,obj) 
+            
+            if(team_name and cup_num and not total_val and not home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (t.Isim = %s AND kupaSayisi = %s)
+                        """
+                obj = (team_name,cup_num)
+                cursor.execute(query,obj)             
+            
+            if(team_name and not cup_num and total_val and not home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (t.Isim = %s AND piyasaDegeri = %s)
+                        """
+                obj = (team_name,total_val)
+                cursor.execute(query,obj)             
+            
+            if(team_name and not cup_num and not total_val and home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (t.Isim = %s AND t.Sehir = %s)
+                        """
+                obj = (team_name,home_towns)
+                cursor.execute(query,obj)                  
+            
+            if(not team_name and cup_num and total_val and not home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (kupaSayisi = %s AND piyasaDegeri = %s)
+                        """
+                obj = (cup_num,total_val)
+                cursor.execute(query,obj)               
+            
+            if(not team_name and cup_num and not total_val and home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (kupaSayisi = %s AND t.Sehir = %s)
+                        """
+                obj = (cup_num,home_towns)
+                cursor.execute(query,obj)               
+            
+            if(not team_name and not cup_num and total_val and home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (piyasaDegeri = %s AND t.Sehir = %s)
+                        """
+                obj = (total_val,home_towns)
+                cursor.execute(query,obj)                 
+
+            if(team_name and cup_num and total_val and not home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (t.Isim = %s AND kupaSayisi = %s AND piyasaDegeri = %s)
+                        """
+                obj = (team_name, cup_num, total_val)
+                cursor.execute(query,obj)                 
+            
+            if(team_name and cup_num and not total_val and home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (t.Isim = %s AND kupaSayisi = %s AND t.Sehir = %s)
+                        """
+                obj = (team_name, cup_num, home_towns)
+                cursor.execute(query,obj)    
+
+            if(team_name and not cup_num and total_val and home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (t.Isim = %s AND piyasaDegeri = %s AND t.Sehir = %s)
+                        """
+                obj = (team_name, total_val, home_towns)
+                cursor.execute(query,obj)    
+
+            if(not team_name and cup_num and total_val and home_towns):
+                query = """ SELECT t.Isim, t.Sehir, t.Stadyum, COUNT(DISTINCT k.KupaSahibi,k.KupaId) AS `kupaSayisi`, SUM(o.PiyasaDegeri) AS `piyasaDegeri`
+                            FROM TAKIM `t` left outer join KUPA `k` on (t.Isim = k.KupaSahibi)
+                                inner join OYUNCU `o` on (t.Isim = o.Takim)
+                                    inner join INSAN `i` on(i.Tc = o.OyuncuTc)
+                            GROUP BY (t.Isim) HAVING (kupaSayisi = %s AND piyasaDegeri = %s AND t.Sehir= %s)
+                        """
+                obj = ( cup_num, total_val,home_towns)
+                cursor.execute(query,obj)    
+
             cursor.connection.commit()
             result = cursor.fetchall()
             return result
